af3a91ad9edc1d6c0f6f07f4a0a04414aa5b8971
Nicci Kaufman <niccikaufman@gmail.com>
2021-03-19 12:55:17 -0700

    add jquery-3.6.0.js

--- js/scripts.js
+++ js/scripts.js
@@ -24,15 +24,15 @@ function convert(number1, number2) {
   return (number1 * 9/5) + number2;
 }
 
-//convert fahrenheit to celcius
+//convert fahrenheit to celcius where number2 should be 32
 function convert2(number1, number2){
   return (number1 - number2) * 5/9;
 }
 
-
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = convert2(number1, number2);
-alert('The total is: ' + result + '.');
+//convert celcius to fahrenheit where number2 should be 32
+// const number1 = parseInt(prompt("Enter a number:"));
+// const number2 = parseInt(prompt("Enter another number:"));
+// const result = convert2(number1, number2);
+// alert('The total is: ' + result + '.');
 
 
