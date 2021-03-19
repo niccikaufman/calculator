a7c5d86d9c9c7240b607f5afc80ab645aaf9221f
Nicci Kaufman <niccikaufman@gmail.com>
2021-03-19 14:08:50 -0700

    update UI and business logic for addition function

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,29 @@
 
-//multiplication
-function multiply(number1, number2) {
-  return number1 * number2;
-}
-
-//addition
+// Business Logic:
 function add(number1, number2) {
   return number1 + number2;
 }
 
-//division
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
 function divide(number1, number2) {
   return number1 / number2;
 }
 
-//subtraction
 function subtract(number1, number2) {
   return number1 - number2;
 }
 
-//convert celius to fahrenheit
-function convert(number1, number2) {
-  return (number1 * 9/5) + number2;
-}
+// Everything below this line is UI logic:
 
-//convert fahrenheit to celcius where number2 should be 32
-function convert2(number1, number2){
-  return (number1 - number2) * 5/9;
-}
-
-//convert celcius to fahrenheit where number2 should be 32
-// const number1 = parseInt(prompt("Enter a number:"));
-// const number2 = parseInt(prompt("Enter another number:"));
-// const result = convert2(number1, number2);
-// alert('The total is: ' + result + '.');
-
-
+$(document).ready(function() {
+  $("form#add").submit(function(event){
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2)
+    $("#output").text(result);
+  });
+});
